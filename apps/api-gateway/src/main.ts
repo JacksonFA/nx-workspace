751fc40d83e0b774e5adbd28898207cd4e9ca140
JacksonFA <jackson.f205@gmail.com>
2023-04-29 17:29:21 -0300

    update api-gateway

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -15,7 +15,7 @@ async function bootstrap() {
   const port = process.env.PORT || 3333;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`,
+    `🚀 Application is running on: https://test1-4lba.onrender.com/${globalPrefix}`,
     'API-GATEWAY'
   );
 }
